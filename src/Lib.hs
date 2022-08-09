e8eaf88f510d4da31c7ba394e10e4544a1f20b13
Daniel Nazarian <danieln@mercury.com>
2022-08-09 15:42:28 -0400

    initial stack code - runDB, runAppT, etc.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,23 @@ module Lib
     ( someFunc
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Pool
+import Database.Persist.Sqlite
+import Control.Monad.Reader
+
+
+data Env = Env { envPool :: Pool SqlBackend }
+
+
+newtype AppT a = AppT { unAppT :: ReaderT Env IO a }
+  deriving newtype (Functor, Applicative, Monad, MonadReader Env, MonadIO)
+  
+  
+runAppT :: MonadIO m => AppT a -> Env -> m a
+runAppT body env = liftIO $ runReaderT (unAppT body) env
+
+
+runDB :: ReaderT SqlBackend IO a -> AppT a
+runDB body = do
+  pool <- asks envPool
+  liftIO $ runSqlPool body pool
