c5d4f323a4c56bbe003d209fdec8368c4e36b972
Daniel Nazarian <danieln@mercury.com>
2022-08-09 17:32:59 -0400

    new types for sqlpersist and db a

--- src/Lib.hs
+++ src/Lib.hs
@@ -22,3 +22,21 @@ runDB :: ReaderT SqlBackend IO a -> AppT a
 runDB body = do
   pool <- asks envPool
   liftIO $ runSqlPool body pool
+
+
+-- TODO Try defining both SqlPersistT m a and DB a and try rewriting runDB in terms of them
+
+-- define both SqlPersistT m a and DB a
+-- SqlPersistT :: (MonadIO m ) => ReaderT SqlBackend m a -> AppT a
+-- SqlPersistT reader = do
+--   pool <- asks envPool
+--   liftIO $ runSqlPool reader pool
+
+newtype SqlPersistT m a = SqlPersistT { unSqlPersistT :: ReaderT SqlBackend m a }
+  deriving newtype (Functor, Applicative, Monad, MonadReader Env, MonadIO)
+
+newtype DB a = DB { unDB :: AppT a }
+  deriving newtype (Functor, Applicative, Monad, MonadReader Env, MonadIO)
+
+
+-- rewrite runDB in terms of SqlPersistT
